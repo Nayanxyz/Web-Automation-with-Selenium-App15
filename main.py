9a9abafb523b78609628ccc89a6870dd2507b14b
NayanPython <choudharyji527@gmail.com>
2026-03-26 14:36:08 +0530

    Automate clicking on Elements and Text Box to open text box interface

--- main.py
+++ main.py
@@ -5,22 +5,35 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-
+# Define driver, options and service
 chrome_options = Options()
 chrome_options.add_argument("--disable-search-engine-choice-screen")
-
 
 service = Service("chromedriver-win64/chromedriver.exe")
 driver = webdriver.Chrome(options=chrome_options,service=service)
 
+# Load the webpage
 driver.get("https://demoqa.com/login")
 
-
+# Locate username , password and login button
 username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
 password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
+login_button = driver.find_element(By.ID, 'login')
 
+
+# Fill in username and password , and click login button
 username_field.send_keys('Nayanxyz')
-password_field.send_keys('Nayanxsdsdsdyz@001')
+password_field.send_keys('Nayanxyz@001')
+driver.execute_script("arguments[0].click();", login_button)
+
+# Locate the elements breakdown and textbox
+elements = (WebDriverWait(driver, 10)
+            .until(EC.visibility_of_element_located((By.XPATH, '//*[@id="root"]/div/div/div/div[1]/div/div/div[1]/span/div'))))
+elements.click()
+
+text_box = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-0')))
+text_box.click()
+
 
 
 input("press enter to close the browser")
