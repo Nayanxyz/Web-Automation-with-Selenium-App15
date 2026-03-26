49d03c274484bb0c9c5bd9852ee009bc25936726
NayanPython <choudharyji527@gmail.com>
2026-03-26 13:11:16 +0530

    Automate login, by clicking on login button

--- main.py
+++ main.py
@@ -5,23 +5,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-
+# Define driver,options and service
 chrome_options = Options()
 chrome_options.add_argument("--disable-search-engine-choice-screen")
-
 
 service = Service("chromedriver-win64/chromedriver.exe")
 driver = webdriver.Chrome(options=chrome_options,service=service)
 
+# load the webpage
 driver.get("https://demoqa.com/login")
 
-
+# locate username, password and login buttons
 username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
 password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
+login_button = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'login')))
 
+# Fill in username and password, and click the button
 username_field.send_keys('Nayanxyz')
-password_field.send_keys('Nayanxsdsdsdyz@001')
-
+password_field.send_keys('Nayanxyz@005524231')
+login_button.click()
 
 input("press enter to close the browser")
 driver.quit()
