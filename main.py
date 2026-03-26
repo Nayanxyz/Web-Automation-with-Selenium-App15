4c0480748da47d4470190f70b1fcfe2ee61f1e83
NayanPython <choudharyji527@gmail.com>
2026-03-26 12:54:40 +0530

    Automate fillup username and password to USERNAME and PASSWORD objects on the webpage

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 
 chrome_options = Options()
 chrome_options.add_argument("--disable-search-engine-choice-screen")
@@ -10,6 +14,14 @@ service = Service("chromedriver-win64/chromedriver.exe")
 driver = webdriver.Chrome(options=chrome_options,service=service)
 
 driver.get("https://demoqa.com/login")
+
+
+username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
+
+username_field.send_keys('Nayanxyz')
+password_field.send_keys('Nayanxsdsdsdyz@001')
+
 
 input("press enter to close the browser")
 driver.quit()
